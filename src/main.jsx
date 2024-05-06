d38b41d4e692f8134331e2a2af839b59792a2d75
david.thomann <david.thomann01@gmail.com>
2024-05-06 14:37:22 +0200

    added mui

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index.jsx";
+import 'semantic-ui-css/semantic.min.css'
 
 export default function App() {
   return (
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,12 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+
 function Index() {
 
   return (
     <>
-      <h1>Index</h1>
+
     </>
   )
 }
