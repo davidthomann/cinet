194ba94d12656e5c65a0901c8238eae61a953cc3
Cyril Tobler <cyriltoibler@icloud.com>
2024-05-08 15:19:47 +0200

    Add NotFound page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header';
+
+function NotFound() {
+    return (
+        <>
+            <Header title="Not Found"/>
+        </>
+    );
+}
+
+export default NotFound;
