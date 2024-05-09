0665c1168a22c91100604bc3aad009ff07f4ed76
Cyril Tobler <cyriltoibler@icloud.com>
2024-05-09 18:09:48 +0200

    Add profile picture after LogIn

--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,32 @@ import Index from "./pages/Index.jsx";
 import Map from "./pages/Map.jsx";
 import PointShop from "./pages/PointShop.jsx";
 import Games from "./pages/Games.jsx";
+import Login from "./pages/auth/Login.jsx";
+import Registration from "./pages/auth/Registration.jsx";
 import 'semantic-ui-css/semantic.min.css'
 import NotFound from "./pages/NotFound.jsx";
+import Context from './Context.js';
+import {useState} from "react";
 
 export default function App() {
+    const [user, setUser] = useState(null);
+
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Index/>}/>
-                <Route path="/map" element={<Map/>}/>
-                <Route path="/points" element={<PointShop/>}/>
-                <Route path="/games" element={<Games/>}/>
+            <Context.Provider value={{user, setUser}}>
+                <Routes>
+                    <Route path="/" element={<Index/>}/>
+                    <Route path="/map" element={<Map/>}/>
+                    <Route path="/points" element={<PointShop/>}/>
+                    <Route path="/games" element={<Games/>}/>
 
-                <Route path="*" element={<Navigate to="/404" />} />
-                <Route path="/404" element={<NotFound />} />
-            </Routes>
+                    <Route path="/auth/login" element={<Login/>}/>
+                    <Route path="/auth/registration" element={<Registration/>}/>
+
+                    <Route path="*" element={<Navigate to="/404" />} />
+                    <Route path="/404" element={<NotFound />} />
+                </Routes>
+            </Context.Provider>
         </BrowserRouter>
     );
 }
---
+++ src/pages/auth/Registration.jsx
@@ -0,0 +1,11 @@
+import AuthHeader from '../../components/AuthHeader';
+
+function Registration() {
+    return (
+        <>
+            <AuthHeader title="Account erstellen" text="Hast du schon einen Account? Hier anmelden!"/>
+        </>
+    );
+}
+
+export default Registration;
