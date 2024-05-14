842be4b6afff5e3365da25e04918202db8d5cece
fhaas <fynngian@gmail.com>
2024-05-14 09:21:38 +0200

    Changed: Login and Registration finished add zurueck Knopf

--- src/pages/auth/Registration.jsx
+++ src/pages/auth/Registration.jsx
@@ -1,33 +1,31 @@
 import AuthHeader from '../../components/AuthHeader';
-import {Stack} from "@mui/joy";
+import {Button, Stack} from "@mui/joy";
 import SubmitButton from "../../components/SubmitButton.jsx"
 import InputField from "../../components/InputField.jsx";
 import {useContext} from "react";
-import {useNavigate} from "react-router-dom";
 import Context from "../../Context.js";
 
 function Registration() {
     const {setUser} = useContext(Context);
-    const navigate = useNavigate();
 
     return (
         <>
             <AuthHeader title="Account erstellen" text="Hast du schon einen Account? Hier anmelden."/>
 
-            {/* Test Code */}
             <form
                 onSubmit={(event) => {
                     event.preventDefault();
                     const formData = new FormData(event.currentTarget);
                     const formJson = Object.fromEntries(Array.from(formData.entries()));
 
-                    const {firstname, lastname} = formJson;
+                    const {firstname, lastname, email, password, birthday} = formJson;
                     setUser({
                         firstname,
                         lastname,
+                        email,
+                        password,
+                        birthday
                     })
-
-                    navigate('/')
                 }}>
                 <Stack spacing={1}>
                     <InputField labelName="Vorname" placeholderText="John" type=""/>
@@ -36,7 +34,9 @@ function Registration() {
                     <InputField labelName="Passwort" placeholderText="Passwort" type="password"/>
                     <InputField labelName="Geburtstag" placeholderText="Geburtstag" type="date"/>
                     <br/>
-                    <SubmitButton text="Registrieren">Submit</SubmitButton>
+                    <SubmitButton text="Registrieren"></SubmitButton>
+                    <br/>
+                    <SubmitButton text="zurück..."></SubmitButton>
                 </Stack>
             </form>
             <br/>
