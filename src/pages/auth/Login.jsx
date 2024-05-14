3dbde496e15a29f6645e3dc5d503986ab5988990
fhaas <fynngian@gmail.com>
2024-05-14 09:21:38 +0200

    Changed: Login and Registration finished add zurueck Knopf

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,11 +1,39 @@
 import AuthHeader from '../../components/AuthHeader';
+import {Stack} from "@mui/joy";
+import InputField from "../../components/InputField.jsx";
+import SubmitButton from "../../components/SubmitButton.jsx";
+import {useContext} from "react";
+import Context from "../../Context.js";
 
-function Index() {
+function Login() {
+    const setUser = useContext(Context)
     return (
         <>
             <AuthHeader title="Login" text="Melde dich an, um fortzufahren."/>
+
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    const formData = new FormData(event.currentTarget);
+                    const formJson = Object.fromEntries(Array.from(formData.entries()));
+
+                    const {email, password} = formJson;
+                    setUser({
+                        email,
+                        password,
+                    })
+
+                }}>
+                <Stack spacing={1}>
+                    <InputField labelName="E-Mail" placeholderText="john.doe@gmail.com" type=""/>
+                    <InputField labelName="Passwort" placeholderText="Passwort" type="password"/>
+                    <SubmitButton text="Einloggen"></SubmitButton>
+                    <br/>
+                    <SubmitButton text="zurück..."></SubmitButton>
+                </Stack>
+            </form>
         </>
     );
 }
 
-export default Index;
+export default Login;
