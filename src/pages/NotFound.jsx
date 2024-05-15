a906e64b3cf4be754b3e4c1e046cb9bd345cfd96
Cyril Tobler <cyriltoibler@icloud.com>
2024-05-15 13:01:43 +0200

    Redesign Error screen

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,21 @@
 import Header from '../components/Header';
+import Typography from "@mui/joy/Typography";
 
 function NotFound() {
     return (
         <>
             <Header title="Not Found"/>
+            <img src="/undraw/error.svg" style={{
+                width: "20rem",
+                maxWidth: "80%",
+                marginLeft: "50vw",
+                marginTop: "3rem",
+                transform: "translateX(-50%)",
+            }}></img>
+            <Typography level="p" sx={{
+                textAlign: "center",
+                marginTop: "2rem"
+            }}>Diese Seite wurde nicht gefunden.</Typography>
         </>
     );
 }
