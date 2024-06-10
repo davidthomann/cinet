f84a295eb64481ee441c5ec914281fd6e92f0e3e
Cyril Tobler <cyriltoibler@icloud.com>
2024-06-10 15:50:00 +0200

    updated Index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,11 +3,15 @@ import Header from '../components/Header';
 import Box from "@mui/joy/Box";
 import { Link } from "react-router-dom";
 import { Button, extendTheme, ThemeProvider } from "@mui/joy";
+import {useContext} from "react";
+import Context from "../Context.js";
 
 function Index() {
+    const { user } = useContext(Context);
+
     return (
         <>
-            <Header />
+            <Header/>
             <Box sx={{
                 display: "flex",
                 flexDirection: "column",
@@ -35,21 +39,22 @@ function Index() {
                 marginTop: '2rem'
             }}>
                 <img src="/images/home_image_banner.png" alt="Home Image Banner" style={{ width: '100%', height: 'auto' }} />
-                <Box sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    gap: "1rem",
-                    marginTop: "5",
-                }}>
-                    <Link to="/auth/login">
-                        <Button color="primary">Anmelden</Button>
-                    </Link>
-                    <Link to="/auth/registration">
-                        <Button color="primary" variant="outlined">Registrieren</Button>
-                    </Link>
-                </Box>
+                {!user &&(
+                    <Box sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        gap: "1rem",
+                        padding: "1rem",
+                    }}>
+                        <Link to="/auth/login">
+                            <Button color="primary">Anmelden</Button>
+                        </Link>
+                        <Link to="/auth/registration">
+                            <Button color="primary" variant="outlined">Registrieren</Button>
+                        </Link>
+                    </Box>
+                )};
             </Box>
-
         </>
     );
 }
